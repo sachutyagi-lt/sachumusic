9325128b77fd370797b919a0ae7e2f925e6d4ae3
UNIQUEGURI <81014154+UNIQURGURI@users.noreply.github.com>
2021-04-15 10:40:19 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ from os import getenv
 from dotenv import load_dotenv
 
 load_dotenv()
-
+que = {}
 SESSION_NAME = getenv("SESSION_NAME", "session")
 BOT_TOKEN = getenv("BOT_TOKEN")
 BOT_NAME = getenv("BOT_NAME")
