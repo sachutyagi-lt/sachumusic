d8b84a1e6c747f9a3f9d3cdbf6e39251b4e95c87
UNIQUEGURI <81014154+UNIQUEGURI@users.noreply.github.com>
2021-04-26 17:18:46 +0530

    Update start.py

--- handlers/start.py
+++ handlers/start.py
@@ -14,32 +14,28 @@ async def start_(client: Client, message: Message):
                 f""" <b>Hey {message.from_user.first_name}!  I can play music in voice chats of Telegeam Groups. I have a lot of cool feature that will amaze you!\n\n🔴 Do you want me to play music in your Telegram groups'voice chats? Please click the \'📜 User Manual 📜\' button below to know how you can use me.\n\n🔴 The [Assistant](https://t.me/Chatuniversemusic1) must be in your group to play music in the voice chat of your group.\n\n🔴 More info & commands mentioned in the [User Manual](https://telegra.ph/Chatuniversemusic1bot-04-14)\n\nA project by @UNIQUEGURI 🇮🇳\n\n✅ Join our bots channel and group\n🔰@Hindi_chattinggg_IND 
  </b>""",
       
-       
-        reply_markup=InlineKeyboardMarkup(
-            [
+       reply_markup=InlineKeyboardMarkup(
+            [ 
                 [
                     InlineKeyboardButton(
-                              " 📜 User Manual 📜", url="https://telegra.ph/Chatuniversemusic1bot-04-14")
-                 
+                        "📜 User Manual 📜", url="https://telegra.ph/Chatuniversemusic1bot-04-14")
+                  ],[
+                    InlineKeyboardButton(
+                        "👨‍💻 Owner 👨‍💻", url="https://t.me/UNIQUEGURI"
                     )
-                ],
-                [
+                   ],[
                     InlineKeyboardButton(
-                                  "Support channel🎙️" , url="https://t.me/cuXmusic"
-                    ),
+                        "Support channel🎙️" , url="https://t.me/cuXmusic"
+                  )
+                ],[ 
                     InlineKeyboardButton(
-                                      "Support Chat 🎙️", url="https://t.me/Hindi_chattinggg_IND"
-                    ),
-                    InlineKeyboardButton(
-                        "⚪owner⚪", url="https://t.me/UNIQUEGURI"
-                    )
-                ],
-                
-                    ) 
-                
+                        "Support Chat 🎙️", url="https://t.me/Hindi_chattinggg_IND"
+                    )]
             ]
-        )
+        ),
+     disable_web_page_preview=True
     )
+        
 
 @Client.on_message(
     filters.command("start")
